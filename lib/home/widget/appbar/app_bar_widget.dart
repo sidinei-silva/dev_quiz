55032499d0f3f6b272991fb39e7598f57fb81b16
Sidinei Silva <sidinei.silva02@gmail.com>
2021-05-04 20:26:09 -0300

    Criando cardscore

--- lib/home/widget/appbar/app_bar_widget.dart
+++ lib/home/widget/appbar/app_bar_widget.dart
@@ -1,42 +1,52 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widget/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
       : super(
-            preferredSize: Size.fromHeight(250),
-            child: Container(
-              height: 250,
-              decoration: BoxDecoration(gradient: AppGradients.linear),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text.rich(
-                      TextSpan(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
                           text: "Ola, ",
                           style: AppTextStyles.title,
                           children: [
                             TextSpan(
                                 text: "Sidinei Silva",
-                                style: AppTextStyles.titleBold)
-                          ]),
-                    ),
-                    Container(
-                      width: 58,
-                      height: 58,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              "https://avatars.githubusercontent.com/u/20242151?v=4"),
+                                style: AppTextStyles.titleBold),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/20242151?v=4"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ));
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget()),
+              ],
+            ),
+          ),
+        );
 }
