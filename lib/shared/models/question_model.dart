22c26c6c0a10485bf0df6950ca5df83483b5caf5
Sidinei Silva <sidinei.silva02@gmail.com>
2021-05-10 16:55:20 -0300

    Criando modelos da aplicação

---
+++ lib/shared/models/question_model.dart
@@ -0,0 +1,11 @@
+import 'package:DevQuiz/shared/models/answer_model.dart';
+
+class QuestionModel {
+  final String title;
+  final List<AnswerModel> answers;
+
+  QuestionModel({
+    required this.title,
+    required this.answers,
+  }) : assert(answers.length == 4);
+}
---
+++ lib/shared/models/quiz_model.dart
@@ -0,0 +1,19 @@
+import 'package:DevQuiz/shared/models/question_model.dart';
+
+enum Level { facil, medio, dificil, perito }
+
+class QuizModel {
+  final String title;
+  final List<QuestionModel> questions;
+  final int questionAnswered;
+  final String image;
+  final Level level;
+
+  QuizModel({
+    required this.title,
+    required this.questions,
+    this.questionAnswered = 0,
+    required this.image,
+    required this.level,
+  });
+}
