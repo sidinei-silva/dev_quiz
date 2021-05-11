5b7fbb29bad556c480b8cfcac98e53cc2706bbab
Sidinei Silva <sidinei.silva02@gmail.com>
2021-05-11 18:04:54 -0300

    Usando splash page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,3 @@
-import 'package:DevQuiz/challenge/challenge_page.dart';
-import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
